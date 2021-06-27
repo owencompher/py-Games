7677f71490a4d8941e912e99820a3ff3a9a7e62f
Owen <owencompher@outlook.com>
2021-06-26 22:15:09 -0400

    added directions for user inputs

--- play_battleships.py
+++ play_battleships.py
@@ -1,4 +1,5 @@
 import Battleships
+input("Input guesses in 'column row' format.\nPress ENTER to begin")
 while True:
     Battleships.Game().play()
     if input("New game? y/n: ") == "y":
--- play_hangman.py
+++ play_hangman.py
@@ -1,4 +1,5 @@
 import Hangman
+input("Guess letters with a single lowercase or the word in all caps.\nPress ENTER to begin")
 while True:
 	Hangman.Game().play()
 	if input("New game? y/n: ") == "n":
